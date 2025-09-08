b6a09c39bbcfc6a45a12d663c6f458f71eeba7ac
vivkweb <vivkvw@gmail.com>
2025-09-08 03:38:21 +0300

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`, // حط مفتاحك بالمتغير
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
